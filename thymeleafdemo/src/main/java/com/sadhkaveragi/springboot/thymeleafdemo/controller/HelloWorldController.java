0f09e6de4495955976ee18ab2c363abb006e6e92
Arjun Singh Bist <arjunsbist@gmail.com>
2024-05-29 17:44:30 +0530

    Implemented reading form data in controller using RequestParam

--- thymeleafdemo/src/main/java/com/sadhkaveragi/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/sadhkaveragi/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -4,26 +4,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // controller method to show initial HTML form
-
     @RequestMapping("/showForm")
     public String showFarm() {
         return "helloworld-form";
     }
 
     // controller method to process the HTML form
-
     @RequestMapping("/processForm")
     public String processForm() {
         return "helloworld";
     }
 
     // controller method to read form data and adding data to model
-
     @RequestMapping("/processForm/v2")
     public String letsShoutDude(HttpServletRequest request, Model model) {
 
@@ -39,6 +37,15 @@ public class HelloWorldController {
         // add message to the model
         model.addAttribute("message", message);
 
+        return "helloworld";
+    }
+
+    // controller method to read form data using RequestParam
+    @RequestMapping("/processForm/v3")
+    public String processFormV3(@RequestParam("studentName") String name, Model model) {
+
+        String message = "Hey My Friend from V3: " + name.toUpperCase();
+        model.addAttribute("message", message);
         return "helloworld";
     }
 }
