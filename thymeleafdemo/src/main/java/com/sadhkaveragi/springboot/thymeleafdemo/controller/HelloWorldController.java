eeef0ea25cef8e4067ddfa7c3aaca617e761e78d
Arjun Singh Bist <arjunsbist@gmail.com>
2024-05-29 17:38:25 +0530

    Implemented reading form data and adding data to Model in Controller

--- thymeleafdemo/src/main/java/com/sadhkaveragi/springboot/thymeleafdemo/controller/HelloWorldController.java
+++ thymeleafdemo/src/main/java/com/sadhkaveragi/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.sadhkaveragi.springboot.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +22,23 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    // controller method to read form
+    // controller method to read form data and adding data to model
+
+    @RequestMapping("/processForm/v2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from the html form
+        String name = request.getParameter("studentName");
+
+        // convert to all uppercase
+        name = name.toUpperCase();
+
+        // create message
+        String message = "Yo! " + name;
+
+        // add message to the model
+        model.addAttribute("message", message);
+
+        return "helloworld";
+    }
 }
